b051d604a544e393a03423858ecc1ea56ad06de5
User <yingkei56@gmail.com>
2020-01-05 21:36:58 +0800

    Done

---
+++ app/src/main/java/com/example/epfcalculator/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.epfcalculator
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        txtDateOfBirth.setOnClickListener() {
+            val c: Calendar = Calendar.getInstance();
+            val currentDay = c.get(Calendar.DAY_OF_MONTH);
+            val currentMonth = c.get(Calendar.MONTH);
+            val currentYear = c.get(Calendar.YEAR);
+
+            val dpd = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                    txtDateOfBirth.setText(
+                        day.toString() + "/" +
+                                (month + 1).toString() + "/" +
+                                year.toString()
+                    )
+
+                    val age = currentYear - year
+                    val basicSaving = getSaving(age)
+
+                    txtAge.text = age.toString()
+                    txtMinBasicSaving.text = "RM " + basicSaving.toString()
+                    txtAllowableInvestment.text = "RM " +
+                            (basicSaving * 0.30).toString()
+
+                }, currentYear, currentMonth, currentDay
+            )
+            dpd.show()
+        }
+    }
+
+    fun getSaving(age: Int): Double {
+        when (age) {
+            in 16..20 -> return 5000.00
+            in 21..25 -> return 14000.00
+            in 26..30 -> return 29000.00
+            in 31..35 -> return 50000.00
+            in 36..40 -> return 78000.00
+            in 41..45 -> return 116000.00
+            in 46..50 -> return 165000.00
+            in 51..55 -> return 228000.00
+
+            else -> return 0.00
+        }
+
+    }
+}
+
